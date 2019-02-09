738a0e00f505eb0440d41858fb532e0b372e8dab
Samuel Young <syoung5@harding.edu>
2019-02-08 18:55:08 -0600

    add logo, logo.js, logo.css

---
+++ website/src/components/logo/logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import logo from '../../images/personal_brand_logo_black.svg';
+import './logo.css';
+
+const Logo = () => (
+    <img src={logo} alt="logo" height="100" width="100" className="" />
+)
+
+export default Logo;
--- website/src/components/title.js
+++ website/src/components/title.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Logo from './logo/logo';
+
 const Title = () => (
     <div>
+        <div>
+            <Logo />
+        </div>
         <h1>Samuel Young</h1>
         <p>Developer | Designer | Adventurer</p>    
     </div>
