96b38359541e3126f805952595b9414c7a19e05d
Samuel Young <syoung5@harding.edu>
2019-02-15 20:38:22 -0600

    add gallery.js, gallery.css, and galleryItem.js

---
+++ website/src/components/gallery/gallery.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Gallery = props => (
+    <div className="gallery">
+        {/*need to map through all the GalleryItems here */}
+    </div>
+)
+
+export default Gallery;
---
+++ website/src/components/gallery/galleryItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GalleryItem = props => (
+    <div>
+
+    </div>
+)
+
+export default GalleryItem
