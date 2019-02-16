397c154fd1cf3e0cabf27274cb98c8e16e45aff2
Samuel Young <syoung5@harding.edu>
2019-02-15 21:41:03 -0600

    update size of gallery items

--- website/src/components/gallery/gallery.js
+++ website/src/components/gallery/gallery.js
@@ -5,18 +5,17 @@ import GalleryItem from './galleryItem/galleryItem';
 import './gallery.css';
 
 import Image from '../../images/5.jpg';
+const images = [];
+images.push(Image, Image, Image, Image, Image, Image, Image, Image, Image, Image);
 
 const Gallery = props => (
     <div className="gallery">
         {/*need to map through all the GalleryItems here */}
-        <GalleryItem img={Image} />
-        <GalleryItem img={Image} />
-        <GalleryItem img={Image} />
-        <GalleryItem img={Image} />
-        <GalleryItem img={Image} />
-        <GalleryItem img={Image} />
-        <GalleryItem img={Image} />
-        <GalleryItem img={Image} />
+        {images.map(img => {
+            return (<GalleryItem 
+                img={img}
+                key={img} />)
+        })}
     </div>
 )
 
