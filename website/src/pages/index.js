35d349dce78eced42d9b050b89773bd4013ea088
Samuel Young <syoung5@harding.edu>
2019-02-15 19:33:17 -0600

    update logo and index.js

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -6,16 +6,26 @@ import Image from "../components/image";
 import SEO from "../components/seo";
 import Title from "../components/title";
 import Logo from '../components/logo/logo';
+import Card from '../components/card/card';
+
+import './index.css';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <Logo width="300" height="300" />
-    <Title />
+
+    <div className="header">
+      <Logo width="300" height="300" />
+      <Title />
+    </div>
+    
+    
+    {/* <Card name="hello" /> */}
+    
     {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div> */}
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
