9b7142eec5e2f7ccff3681d7b8c87ac77ac6c3fa
Samuel Young <syoung5@harding.edu>
2019-02-17 01:56:01 -0600

    add contact section

---
+++ website/src/components/contact/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './contact.module.css';
+
+const Contact = () => (
+    <p className={styles.contact}>
+        Get in touch
+        <span className={styles.email}>syoung5@harding.edu</span> 
+    </p>
+)
+
+export default Contact;
