b1ca03263960e79141a213fde30d05be93bb26e5
Samuel Young <syoung5@harding.edu>
2019-02-16 23:50:14 -0600

    center title

---
+++ website/src/components/title/title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './title.module.css';
+
+const Title = () => (
+    <div className={styles.title}>
+        <h1>Samuel Young</h1>
+        <p>Developer | Designer | Adventurer</p>    
+    </div>
+)
+
+export default Title;
