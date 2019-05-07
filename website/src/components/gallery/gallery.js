736dd6de44bab6bcadcae053709d645064d2b705
Samuel Young <syoung5@harding.edu>
2019-05-07 16:17:12 -0500

    Update gallery.js
    add real pictures and change route

--- website/src/components/gallery/gallery.js
+++ website/src/components/gallery/gallery.js
@@ -1,22 +1,29 @@
 import React from 'react';
 
 import GalleryItem from './galleryItem/galleryItem';
-
 import styles from './gallery.module.css';
 
-import Image from '../../images/5.jpg';
+import bookCovers from '../../images/book-mockup-into-the-air.png';
+import Image2 from '../../images/laptop-NOLA.png';
+import Image3 from '../../images/zion-pro-bono-front.jpg';
+import Image4 from '../../images/home-hero.png';
+import Image7 from '../../images/uno-box-meals.png';
+import Image6 from '../../images/Benny-ad-1.png';
+import Image5 from '../../images/dm-poster.png';
+
 const images = [];
-images.push(Image, Image, Image, Image, Image, Image, Image, Image, Image, Image);
+images.push(bookCovers, Image2, Image3, Image4, Image5, Image6, Image7);
 
 const Gallery = props => (
     <React.Fragment>
         <h1 className={styles.title}>Work</h1>
         <div className={styles.gallery}>
-            {/*need to map through all the GalleryItems here */}
             {images.map(img => {
                 return (<GalleryItem 
                     img={img}
-                    key={img} />)
+                    key={img}
+                    to={`/work/`} 
+                    state={{ image: img }}/>)
             })}
         </div>
     </React.Fragment>
