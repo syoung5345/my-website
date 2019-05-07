324a2768b91e9e654fc27e5322d2a79a0ca059a5
Samuel Young <syoung5@harding.edu>
2019-05-07 17:59:22 -0500

    Update contact.js
    include mailto link

--- website/src/components/contact/contact.js
+++ website/src/components/contact/contact.js
@@ -5,7 +5,7 @@ import styles from './contact.module.css';
 const Contact = () => (
     <p className={styles.contact}>
         Get in touch
-        <span className={styles.email}>syoung5@harding.edu</span> 
+        <a href="mailto:syoung5@harding.edu" className={styles.email}>syoung5@harding.edu</a> 
     </p>
 )
 
