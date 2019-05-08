0513aad7e43580e1d183590c56bf841fd14d40f0
Samuel Young <syoung5@harding.edu>
2019-05-07 21:37:32 -0500

    Update work4.js

---
+++ website/src/pages/work4.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout/layout';
+
+import styles from './index.module.css';
+
+import home from '../images/cotopaxi-home.png';
+import libre from '../images/libre.png';
+import libreDetails from '../images/libre-details.png';
+
+const Work3 = props => (
+    <Layout>
+        <div className={styles.work}>
+            <Link to="/#work">back</Link>
+            <h1>Cotopaxi Website</h1>
+            <p>
+                This redesign was focused on bringing stronger visual impact and more user engagement to interest the viewer right away.
+            </p>
+            <img src={home} alt="home"/>
+            <img src={libre}  alt="libre"/>
+            <img src={libreDetails}  alt="libre details"/>
+        </div>
+    </Layout>
+)
+
+export default Work3;
