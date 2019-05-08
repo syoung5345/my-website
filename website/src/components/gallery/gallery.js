e4f0c7a05f2504e8d18f5da627a32963765b201c
Samuel Young <syoung5@harding.edu>
2019-05-07 22:04:56 -0500

    Update gallery.js
    delete unnecessary code

--- website/src/components/gallery/gallery.js
+++ website/src/components/gallery/gallery.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import GalleryItem from './galleryItem/galleryItem';
 import styles from './gallery.module.css';
@@ -8,14 +7,11 @@ import bookCovers from '../../images/book-mockup-into-the-air.png';
 import Image2 from '../../images/laptop-NOLA.png';
 import Image3 from '../../images/zion-pro-bono-front.jpg';
 import Image4 from '../../images/home-hero.png';
-// import Image5 from '../../images/dm-poster.png';
 import Image5 from '../../images/Benny-ad-1.png';
 import Image6 from '../../images/uno-box-meals.png';
 
 const images = [];
 images.push(bookCovers, Image2, Image3, Image4, Image5, Image6);
-
-let count = 8;
 
 const Gallery = props => (
     <div>
