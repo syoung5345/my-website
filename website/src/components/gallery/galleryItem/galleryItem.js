6b4d27cbcfd890c0673b5fe881c212bb478a5db6
Samuel Young <syoung5@harding.edu>
2019-05-07 22:05:23 -0500

    Update galleryItem.js
    delete code that is no longer needed

--- website/src/components/gallery/galleryItem/galleryItem.js
+++ website/src/components/gallery/galleryItem/galleryItem.js
@@ -4,18 +4,12 @@ import { Link } from 'gatsby';
 
 import styles from './galleryItem.module.css';
 
-import intoThinAir from '../../../images/book-mockup-into-the-air.png';
-import eigerDreams from '../../../images/book-mockup-eiger-dreams.png';
-import intoTheWild from '../../../images/book-mockup-into-the-wild.png';
-
 const GalleryItem = props => (
     <Link 
         className={styles.item}
         to={props.to}>
             <img src={props.img}/>
-            <div className={styles.item__overlay}>
-                <div>View →</div>
-            </div>
+            <div className={styles.view}>View →</div>
    </Link>
 )
 
