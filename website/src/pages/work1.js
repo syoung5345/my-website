36567f66ac440173308e3ed56e81cbe29edd5400
Samuel Young <syoung5@harding.edu>
2019-05-11 14:24:58 -0500

    Update work1.js
    refactor to use Gatsby Image

--- website/src/pages/work1.js
+++ website/src/pages/work1.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 import styles from './index.module.css';
-
-import intoThinAir from '../images/book-mockup-into-the-air.png';
-import eigerDreams from '../images/book-mockup-eiger-dreams.png';
-import intoTheWild from '../images/book-mockup-into-the-wild.png';
 
 const Work1 = props => (
         <div className={styles.work}>
@@ -16,10 +13,34 @@ const Work1 = props => (
             <p>
             The concept for these book covers was to combine nature with a man-made item used in each of the books. In all of these books, people are trying to overcome nature in one way or another. These covers are portraying this idea. The different items represent the people in each story. It appears as though the man-made items are overpowering the elements of nature because of their size, or are they? That is the question that the reader has to decide for each story.
             </p>
-            <img src={intoThinAir} alt="book cover"/>
-            <img src={intoTheWild} alt="book cover1"/>
-            <img src={eigerDreams} alt="book cover2"/>
+            <Img fluid={props.data.Book1.childImageSharp.fluid} />
+            <Img fluid={props.data.Book2.childImageSharp.fluid} />
+            <Img fluid={props.data.Book3.childImageSharp.fluid} />
         </div>
 )
 
 export default Work1;
+
+export const fluidImage = graphql `
+fragment fluidImage on File {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+}
+`;
+
+export const pageQuery = graphql `
+query {
+    Book1: file(relativePath: { eq: "book-mockup-into-the-air.png" }) {
+        ...fluidImage
+    }
+    Book2: file(relativePath: { eq: "book-mockup-into-the-wild.png" }) {
+        ...fluidImage
+    }
+    Book3: file(relativePath: { eq: "book-mockup-eiger-dreams.png" }) {
+        ...fluidImage
+    }
+}
+`;
