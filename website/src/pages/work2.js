1eebc58a98954e8da3e56e51d8c1b8daee0900d4
Samuel Young <syoung5@harding.edu>
2019-05-11 14:32:34 -0500

    Update work2.js
    update to Gatsby Image

--- website/src/pages/work2.js
+++ website/src/pages/work2.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Layout from '../components/layout/layout';
+import Img from 'gatsby-image';
 
 import styles from './index.module.css';
-
-import home from '../images/NOLA-home.png';
-import explore from '../images/NOLA-explore.png';
-import nightlife from '../images/NOLA-nightlife.png';
 
 const Work2 = props => (
         <div className={styles.work}>
@@ -17,10 +13,34 @@ const Work2 = props => (
             <p>
             This website is a redesign of the New Orleans travel website. I redesigned the tag lines, visuals, and overall brand of the site.
             </p>
-            <img src={home} height="80%" width="80%" alt="NOLA home page"/>
-            <img src={explore} height="80%" width="80%" alt="NOLA explore"/>
-            <img src={nightlife} height="80%" width="80%" alt="NOLA Nightlife"/>
+            <Img fluid={props.data.home.childImageSharp.fluid} />
+            <Img fluid={props.data.explore.childImageSharp.fluid} />
+            <Img fluid={props.data.nightlife.childImageSharp.fluid} />
         </div>
 )
 
 export default Work2;
+
+export const fluidImage2 = graphql `
+fragment fluidImage2 on File {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+}
+`;
+
+export const work3Query = graphql `
+query {
+    home: file(relativePath: { eq: "NOLA-home.png" }) {
+        ...fluidImage2
+    }
+    explore: file(relativePath: { eq: "NOLA-explore.png" }) {
+        ...fluidImage2
+    }
+    nightlife: file(relativePath: { eq: "NOLA-nightlife.png" }) {
+        ...fluidImage2
+    }
+}
+`;
