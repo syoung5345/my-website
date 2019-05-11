46d59efcf0863b38dfc63bfe536ba64d09285115
Samuel Young <syoung5@harding.edu>
2019-05-11 14:44:59 -0500

    Update work5.js
    update to Gatsby Image

--- website/src/pages/work5.js
+++ website/src/pages/work5.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Layout from '../components/layout/layout';
+import Img from 'gatsby-image';
 
 import styles from './index.module.css';
-
-import stickers from '../images/Benny-sticker-mockup.png';
-import ads from '../images/Benny-ads.png';
 
 const Work5 = props => (
         <div className={styles.work}>
@@ -17,9 +14,30 @@ const Work5 = props => (
             This project was a collaboration. My team consisted of Kaylee Lockenour, Haydin Hall, and Sydney Duckels, and myself. The goal was to create a logo and brand identity, way finding, ads, and event promotions for biking in the city of Searcy, AR. 
                  I worked on the logo, way finding, and some of the ads. We brainstormed our ideas together and then divded out the work.
             </p>
-            <img src={stickers}  alt="stickers"/>
-            <img src={ads}  alt="ads"/>
+            <Img fluid={props.data.stickers.childImageSharp.fluid} />
+            <Img fluid={props.data.ads.childImageSharp.fluid} />
         </div>
 )
 
 export default Work5;
+
+export const fluidImage5 = graphql `
+fragment fluidImage5 on File {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+}
+`;
+
+export const work4Query = graphql `
+query {
+    stickers: file(relativePath: { eq: "Benny-sticker-mockup.png" }) {
+        ...fluidImage5
+    }
+    ads: file(relativePath: { eq: "Benny-ads.png" }) {
+        ...fluidImage5
+    }
+}
+`;
