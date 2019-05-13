c4cbedcb8c54ec5b245f10c4323c458cdef2a54a
Samuel Young <syoung5@harding.edu>
2019-05-12 22:42:50 -0500

    Update gallery.js
    update to use StaticQuery for the Images

--- website/src/components/gallery/gallery.js
+++ website/src/components/gallery/gallery.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { StaticQuery, graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
 
 import GalleryItem from './galleryItem/galleryItem';
 import styles from './gallery.module.css';
@@ -10,25 +12,100 @@ import Cotopaxi from '../../images/home-hero.png';
 import BikeSearcy from '../../images/Benny-ad-1.png';
 import PSA from '../../images/uno-box-meals.png';
 
-const images = [];
-// images.push(bookCovers, Image2, Image3, Image4, Image5, Image6);
-
-images.push({img: bookCovers, title: 'Book Covers'}, {img: NOLA, title: 'Travel Website'}, 
+const images = [{img: bookCovers, title: 'Book Covers'}, {img: NOLA, title: 'Travel Website'}, 
     {img: Zion, title: 'Zion Brochure'}, {img: Cotopaxi, title: 'Cotopaxi Website'},
-    {img: BikeSearcy, title: 'Bike Searcy Branding'}, {img: PSA, title: 'PSA Child Hunger'} );
+    {img: BikeSearcy, title: 'Bike Searcy Branding'}, {img: PSA, title: 'PSA Child Hunger'}];
 
 const Gallery = props => (
     <div>
         <h1 className={styles.title}>Work</h1>
-        <div className={styles.gallery}>
-            {images.map((img, index, images) => {
-                return (<GalleryItem 
-                            img={img.img}
-                            key={img.img}
-                            to={`/work${index + 1}`} 
-                            title={img.title}/>)
-            })}
-        </div>
+            <StaticQuery 
+                query={graphql `
+                query {
+                    bookCovers: file(relativePath: { eq: "book-mockup-into-the-air.png" }) {
+                        childImageSharp {
+                            fluid(maxWidth: 1000) {
+                              ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
+                    nola: file(relativePath: { eq: "laptop-NOLA.png" }) {
+                        childImageSharp {
+                            fluid(maxWidth: 1000) {
+                              ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
+                    zion: file(relativePath: { eq: "zion-pro-bono-front.jpg" }) {
+                        childImageSharp {
+                            fluid(maxWidth: 1000) {
+                              ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
+                    cotopaxi: file(relativePath: { eq: "home-hero.png" }) {
+                        childImageSharp {
+                            fluid(maxWidth: 1000) {
+                              ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
+                    bikeSearcy: file(relativePath: { eq: "Benny-ad-1.png" }) {
+                        childImageSharp {
+                            fluid(maxWidth: 1000) {
+                              ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
+                    psa: file(relativePath: { eq: "uno-box-meals.png" }) {
+                        childImageSharp {
+                            fluid(maxWidth: 1000) {
+                              ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
+                }`}
+                render={data => (
+                    <div className={styles.Gallery}>
+                        <Link 
+                            className={styles.item}
+                            to={`work1`}>
+                            <Img fluid={data.bookCovers.childImageSharp.fluid} />
+                            <p className={styles.view}>Book Covers</p>
+                        </Link>
+                        <Link 
+                            className={styles.item}
+                            to={`work2`}>
+                            <Img fluid={data.nola.childImageSharp.fluid} />
+                            <p className={styles.view}>Travel Website</p>
+                        </Link>
+                        <Link 
+                            className={styles.item}
+                            to={`work3`}>
+                            <Img fluid={data.zion.childImageSharp.fluid} />
+                            <p className={styles.view}>Zion Brochure</p>
+                        </Link>
+                        <Link 
+                            className={styles.item}
+                            to={`work4`}>
+                            <Img fluid={data.cotopaxi.childImageSharp.fluid} />
+                            <p className={styles.view}>Cotopaxi Website</p>
+                        </Link>
+                        <Link 
+                            className={styles.item}
+                            to={`work5`}>
+                            <Img fluid={data.bikeSearcy.childImageSharp.fluid} />
+                            <p className={styles.view}>Bike Searcy Branding</p>
+                        </Link>
+                        <Link 
+                            className={styles.item}
+                            to={`work6`}>
+                            <Img fluid={data.psa.childImageSharp.fluid} />
+                            <p className={styles.view}>PSA-Child Hunger</p>
+                        </Link>
+                    </div>
+                )}
+            />
     </div>
 )
 
