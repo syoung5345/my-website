ad39c0b8ce62a3c19ff45a9aa86747517cc2bcf5
Samuel Young <syoung5@harding.edu>
2019-08-16 16:57:55 -0500

    Upadate header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,55 +15,34 @@ class Header extends Component {
     };
   }
 
-  // Adds an event listener when the component is mount.
-  // componentDidMount() {
-  //   window.addEventListener("scroll", this.handleScroll);
-  // }
+  //Adds an event listener when the component is mount.
+  componentDidMount() {
+    this.setState({prevScrollpos: window.pageXOffset});
+    window.addEventListener("scroll", this.handleScroll);
+  }
 
-  // // Remove the event listener when the component is unmount.
-  // componentWillUnmount() {
-  //   window.removeEventListener("scroll", this.handleScroll);
-  // }
+  // Remove the event listener when the component is unmount.
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
 
   // Hide or show the menu.
-  // handleScroll = () => {
-  //   const { prevScrollpos } = this.state;
+  handleScroll = () => {
+    const { prevScrollpos } = this.state;
 
-  //   const currentScrollPos = window.pageYOffset;
-  //   const visible = prevScrollpos > currentScrollPos;
+    const currentScrollPos = window.pageYOffset;
+    const visible = prevScrollpos > currentScrollPos;
 
-  //   this.setState({
-  //     prevScrollpos: currentScrollPos,
-  //     visible
-  //   });
-  // };
-
-  // componentDidMount() {
-  //   //this.prevScrollpos = window.pageYOffset;
-  //   window.onscroll = function() {
-  //     this.currentScrollPos = window.pageYOffset;
-  //     if (this.prevScrollpos > this.currentScrollPos) {
-  //       const navbar = document.querySelector(".navbar");
-  //       if (navbar != null) {
-  //         navbar.style.opacity = 1;
-  //       }
-  //     } else {
-  //       const navbar = document.querySelector(".navbar");
-  //       if (navbar != null) {
-  //         navbar.style.opacity = 0;
-  //       }
-  //     }
-  //     this.prevScrollpos = this.currentScrollPos;
-  //   } 
-  // }
+    this.setState({
+      prevScrollpos: currentScrollPos,
+      visible
+    });
+  };
 
   render() {
     return (
       <nav className={`navbar ${this.state.visible ? '' : 'navbar--hidden'}`}>
-        {/* <Link to="/">
-          <Logo width="50" height="50" />
-        </Link> */}
-        {/* <Link to="/#about" className="link">About</Link> */}
+        <Link to="/#about" className="link">About</Link>
         <Link to="/#work" className="link">Work</Link>
         <Link to="/#contact" className="link">Contact</Link>
       </nav>
